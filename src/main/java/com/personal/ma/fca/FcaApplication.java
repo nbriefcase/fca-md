5e7c71d0657982d8c1348b74fc211e0a674048ce
Neiro Diaz <gpbriefcase@gmail.com>
2023-07-02 16:19:30 -0500

    Initial Commit

---
+++ src/main/java/com/personal/ma/fca/FcaApplication.java
@@ -0,0 +1,13 @@
+package com.personal.ma.fca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FcaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/ma/fca/FcaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.ma.fca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
